cfa58b29f97880d34c156384c55068cecef2f070
Hyun-Koo KIM <studian@gmail.com>
2019-10-11 10:39:25 +0900

    Add files via upload

---
+++ cuda10.1-cudnn7.6-devel-ubuntu16.04/install_lib.sh
@@ -0,0 +1,9 @@
+pip install Pillow numpy graphviz scikit-image scikit-learn scipy tqdm numpy matplotlib
+pip install opencv-python opencv-contrib-python
+pip install tensorflow-gpu
+pip install keras
+apt-get install libsm6 libxrender1 libfontconfig1
+pip install --upgrade pip 
+pip install torch==0.4.1 torchvision
+pip install jupyter notebook
+
